e10bbb0a94b12df639f6cd4a5f204b8615f7f2c4
vijayasimhareddy <vijayasimha.reddy@girmiti.com>
2020-07-29 07:59:46 +0530

    Initial Commit

---
+++ WeatherReport/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherReport
+//
+//  Created by Vijay on 29/07/20.
+//  Copyright © 2020 senix.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
